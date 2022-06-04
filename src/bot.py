f9dfb3943403929c79e1b5a19a46f616382891ed
Timothy Barao <tjb13b@my.fsu.edu>
2022-06-04 11:28:54 -0400

    First Iteration of Bot
    Added Clear command
    Added Remove Roles command

--- src/bot.py
+++ src/bot.py
@@ -2,10 +2,28 @@ import os
 
 #import discord
 from discord.ext import commands
+from discord import Intents
 import logging
 
+intents = Intents(messages=True, guilds=True, members=True)
+
+'''
+    ANNOUNCEMENTS
+        1. Clear all messages from pc-announcements channel
+
+    LFG COMMANDS:
+        1. Remove LFG Role from single user
+        2. Remove LFG Role from multiple users
+        TODO: 3. Remove LFG Role from ALL users
+        4. Clear all messages from LFG channel
+
+    COMMANDS RESTRICTIONS
+        1. TODO: Role, channel
+        2. TODO: Verify permissions, delete message if wrong permissions
+'''
 
 logging.basicConfig(level=logging.DEBUG)
+
 '''
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -16,14 +34,71 @@ handler.setFormatter(logging.Formatter(
 logger.addHandler(handler)
 '''
 
+bot = commands.Bot(command_prefix='$', intents = intents)
+
 BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
+GUILD_TOKEN = os.environ.get('GUILD_TOKEN', None)
 
-bot = commands.Bot(command_prefix='$')
-
+''' Tokens for LFG Roles '''
+LOWER_TOKEN = os.environ.get('LOWER_TOKEN', None)
+UPPER_TOKEN = os.environ.get('UPPER_TOKEN', None)
 
 @bot.command()
 async def test(ctx, arg):
     await ctx.send(arg)
 
+@bot.command()
+@commands.has_permissions(manage_roles=True, ban_members=True)
+async def clear(ctx, channel_id):
+    """
+    Clear all messages in a given channel
+
+    Syntax:
+        $clear <channel_id> 
+    
+    Parameters:
+        ctx : Context Class Object
+            Represents the context in which a command is being invoked under.
+        channel_id : string (converted to int for functions)
+            ID of channel to have messages deleted. 
+            Get ID from Discord Server, Right-click Channel, Copy ID
+
+    """
+    channel = bot.get_channel(int(channel_id))
+    msgs = [message async for message in channel.history()]
+    await channel.delete_messages(msgs)
+
+@bot.command()
+@commands.has_permissions(manage_roles=True, ban_members=True)
+async def remove_role(ctx, user_ids, division):
+    """
+    Remove the LFG Roles from Users, LFG_Upper or LFG_Lower
+
+    Syntax:
+        $remove_role <user_ids> <"LFG_Lower"/"LFG_Upper"> 
+    
+    Parameters:
+        ctx : Context Class Object
+            Represents the context in which a command is being invoked under.
+        user_ids: comma separated string, converted to List
+            List of comma separate user IDS to remove the LFG role from.
+            Format of User ID : Name#1234
+        division : String
+            Determine which role to remove, either LFG_Upper or LFG_Lower
+            
+    """
+    guild = bot.get_guild(int(GUILD_TOKEN))
+    user_ids = list(user_ids.split(','))
+
+    if division == "LFG_Lower":
+        role = guild.get_role(int(LOWER_TOKEN))
+    elif division == "LFG_Upper":
+        role = guild.get_role(int(UPPER_TOKEN))
+    else:        
+        role = 404
+
+    for u in user_ids: 
+        user = guild.get_member_named(u)
+        await user.remove_roles(role)
 
 bot.run(BOT_TOKEN)
