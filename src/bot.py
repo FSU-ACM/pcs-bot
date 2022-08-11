9d53bc0188db9946ecce0e085d781b7b0f27abac
Timothy Barao <tjb13b@my.fsu.edu>
2022-08-11 13:41:08 -0400

    Added Build/Teardown commands
    Added new commands:
    
    $clear
    $remove_all_roles
    $create_roles

--- src/bot.py
+++ src/bot.py
@@ -1,29 +1,27 @@
-import os
-
-#import discord
+from multiprocessing.connection import wait
 from discord.ext import commands
-from discord import Intents
+from discord import Intents, utils
 import logging
+import os
 
 intents = Intents(messages=True, guilds=True, members=True)
 
 '''
     ANNOUNCEMENTS
-        1. Clear all messages from pc-announcements channel
+        TODO: 1. Clear all messages from pc-announcements channel
 
     LFG COMMANDS:
         1. Remove LFG Role from single user
         2. Remove LFG Role from multiple users
-        TODO: 3. Remove LFG Role from ALL users
+        3. Remove LFG Role from ALL users
         4. Clear all messages from LFG channel
+        TODO: 5. After clone, move channel to top of list
 
     COMMANDS RESTRICTIONS
-        1. TODO: Role, channel
-        2. TODO: Verify permissions, delete message if wrong permissions
+        1. Role, channel
+        2. Verify permissions, delete message if wrong permissions
 '''
-
 logging.basicConfig(level=logging.DEBUG)
-
 '''
 logger = logging.getLogger('discord')
 logger.setLevel(logging.DEBUG)
@@ -33,25 +31,42 @@ handler.setFormatter(logging.Formatter(
     '%(asctime)s:%(levelname)s:%(name)s: %(message)s'))
 logger.addHandler(handler)
 '''
-
 bot = commands.Bot(command_prefix='$', intents = intents)
 
 BOT_TOKEN = os.environ.get('BOT_TOKEN', None)
 GUILD_TOKEN = os.environ.get('GUILD_TOKEN', None)
 
-''' Tokens for LFG Roles '''
-LOWER_TOKEN = os.environ.get('LOWER_TOKEN', None)
-UPPER_TOKEN = os.environ.get('UPPER_TOKEN', None)
+LOWER = "LFG_Lower"
+UPPER = "LFG_Upper"
 
-@bot.command()
-async def test(ctx, arg):
-    await ctx.send(arg)
+UPPER_CHANNEL = "lfg-upper-division"
+LOWER_CHANNEL = "lfg-lower-division"
 
+# ''' Tokens for LFG Roles '''
+# LOWER_TOKEN = os.environ.get('LOWER_TOKEN', None)
+# UPPER_TOKEN = os.environ.get('UPPER_TOKEN', None)
+
+''' Specify channel name to only allow commands to occur there'''
+BOT_CHANNEL = "bot_commands"
+
+def bot_channel_only():
+    async def predicate(ctx):
+        if ctx.channel.name != BOT_CHANNEL:
+            # Do the handling here or in on_command_error handler
+            message = ctx.message
+            await message.delete()
+            await ctx.send("INCORRECT CHANNEL")
+            #raise commands.CheckFailure("Channel isn't valid!")
+        return True
+
+    return commands.check(predicate)
+
+@bot_channel_only()
 @bot.command()
 @commands.has_permissions(manage_roles=True, ban_members=True)
 async def clear(ctx, channel_id):
     """
-    Clear all messages in a given channel
+    Clones channel with messages deleted, changes channel_ID
 
     Syntax:
         $clear <channel_id> 
@@ -64,15 +79,51 @@ async def clear(ctx, channel_id):
             Get ID from Discord Server, Right-click Channel, Copy ID
 
     """
-    channel = bot.get_channel(int(channel_id))
-    msgs = [message async for message in channel.history()]
-    await channel.delete_messages(msgs)
 
+    channel = bot.get_channel(int(channel_id))
+    if channel is not None:
+        await channel.clone(reason="Has been nuked")
+        await channel.delete()
+    else:
+        await ctx.send(f'No channel was found with id: **{channel_id}**')
+
+@bot_channel_only()
+@bot.command()
+@commands.has_permissions(manage_roles=True, ban_members=True)
+async def remove_all_roles(ctx):
+    """
+    """
+    guild = bot.get_guild(int(GUILD_TOKEN))
+
+    lower_role = utils.get(guild.roles, name=LOWER)
+    upper_role = utils.get(guild.roles, name=UPPER)
+
+    await lower_role.delete()
+    await upper_role.delete()
+
+@bot_channel_only()
+@bot.command()
+@commands.has_permissions(manage_roles=True, ban_members=True)
+async def create_roles(ctx):
+    """
+    """
+    guild = bot.get_guild(int(GUILD_TOKEN))
+    await guild.create_role(name=UPPER)
+    await guild.create_role(name=LOWER)
+
+    # Slower than just getting by ID
+    lfg_upper = utils.get(guild.text_channels, name=UPPER_CHANNEL)
+    lfg_lower = utils.get(guild.text_channels, name=LOWER_CHANNEL)
+    
+    await lfg_upper.set_permissions(utils.get(guild.roles, name=UPPER), read_messages=True, send_messages=True)
+    await lfg_lower.set_permissions(utils.get(guild.roles, name=LOWER), read_messages=True, send_messages=True)
+
+@bot_channel_only()
 @bot.command()
 @commands.has_permissions(manage_roles=True, ban_members=True)
 async def remove_role(ctx, user_ids, division):
     """
-    Remove the LFG Roles from Users, LFG_Upper or LFG_Lower
+    Remove the LFG Roles from a User/list of Users, LFG_Upper or LFG_Lower role
 
     Syntax:
         $remove_role <user_ids> <"LFG_Lower"/"LFG_Upper"> 
@@ -87,25 +138,23 @@ async def remove_role(ctx, user_ids, division):
             Determine which role to remove, either LFG_Upper or LFG_Lower
             
     """
+
     guild = bot.get_guild(int(GUILD_TOKEN))
     user_ids = list(user_ids.split(','))
 
-    if division == "LFG_Lower":
-        role = guild.get_role(int(LOWER_TOKEN))
-    elif division == "LFG_Upper":
-        role = guild.get_role(int(UPPER_TOKEN))
-    else:        
-        role = 404
+    role = utils.get(guild.roles, name=division)
+    # TODO: Error Checking  
 
     for u in user_ids: 
         user = guild.get_member_named(u)
         await user.remove_roles(role)
 
+@bot_channel_only()
 @bot.command()
 @commands.has_permissions(manage_roles=True, ban_members=True)
 async def add_role(ctx, user_ids, division):
     """
-    Add the LFG Roles to Users, LFG_Upper or LFG_Lower
+    Add the LFG Roles to a User/List of Users, LFG_Upper or LFG_Lower
 
     Syntax:
         $add_role <user_ids> <"LFG_Lower"/"LFG_Upper"> 
@@ -123,15 +172,20 @@ async def add_role(ctx, user_ids, division):
     guild = bot.get_guild(int(GUILD_TOKEN))
     user_ids = list(user_ids.split(','))
 
-    if division == "LFG_Lower":
-        role = guild.get_role(int(LOWER_TOKEN))
-    elif division == "LFG_Upper":
-        role = guild.get_role(int(UPPER_TOKEN))
-    else:        
-        role = 404
+    role = utils.get(guild.roles, name=division)
+
+    if division == UPPER:
+        lfg_channel = utils.get(guild.text_channels, name=UPPER_CHANNEL)
+    elif division == LOWER:
+        lfg_channel = utils.get(guild.text_channels, name=LOWER_CHANNEL)
+    else:
+        404
+
+    # TODO: Error Checking  
 
     for u in user_ids: 
         user = guild.get_member_named(u)
         await user.add_roles(role)
+        await lfg_channel.send(f'**{user}** has joined, looking for a group!')
 
 bot.run(BOT_TOKEN)
